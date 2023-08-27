2581a33186648adb5614491d22a1379e43260fe6
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-08-27 18:02:18 -0300

    meu contato pronto

--- src/components/MeuContato.vue
+++ src/components/MeuContato.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="main">
-    <h1>Meu contato</h1>
+    <div class="container">
+      <h2>Meu contato</h2>
+      <div class="texto">
+        <p>Caso tenha interesse no meu trabalho e/ou ache que posso te ajudar, pode entrar em contato comigo através do meu email :D</p>
+        <div class="email">
+          <p>
+            guiapinheiro29@gmail.com
+          </p>
+        </div>
+      </div>
+      <div class="github">
+        <p>
+          Te convido também a dar uma olhada nos meus repositórios do <a href="https://github.com/guilherme-a-pinheiro" target="_blank">GitHub</a>!
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +25,63 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+  }
 
+  .container {
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .container h2 {
+      background: #070303;
+        width: 575px;
+        text-align: center;
+        color: #fff;
+        padding: 6px;
+        border-radius: 10px;
+  }
+
+  .texto p {
+    max-width: 575px;
+    font-size: 24px;
+  }
+
+  .texto {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .email {
+    margin: 20px 0px;
+    font-size: 25px;
+    text-decoration: underline;
+  }
+
+  .email p {
+    background: rgb(128, 208, 128);
+    padding: 10px;
+    border-radius: 10px;
+  }
+
+  .github p {
+    background: gray;
+    padding: 20px;
+    border-radius: 10px;
+  }
+
+  .github p a {
+    text-decoration: none;
+    background: inherit;
+  }
 </style>
